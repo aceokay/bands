d0f292685681cd7cea6dfb12d9ef6f6952f6d84f
Austin <aokincaid@gmail.com>
2015-08-28 10:04:45 -0700

    Add additional functionality to custom classes for normalization of inputs

--- lib/band.rb
+++ lib/band.rb
@@ -1,4 +1,11 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many(:venues)
+  before_validation(:normalize)
   validates(:name, {:presence => true})
+  validates(:name, {:uniqueness => true})
+  
+private
+  define_method(:normalize) do
+    self.name=(name().strip().split(/(\W)/).map(&:capitalize).join)
+  end
 end
