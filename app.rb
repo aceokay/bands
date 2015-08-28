29e1dae93062a1fbf1df4d4f5c91f41332b72e4c
Austin <aokincaid@gmail.com>
2015-08-28 11:59:02 -0700

    Add post and delete route functionality to both bands and venues

--- app.rb
+++ app.rb
@@ -9,8 +9,37 @@ get('/') do
   erb(:index)
 end
 
+get('/bands') do
+  redirect('/')
+end
+
+get('/venues') do
+  @venues = Venue.all()
+  erb(:venue_list)
+end
+
 post('/bands/add') do
   band_name = params.fetch('name')
   Band.create({:name => band_name})
+  redirect back
+end
+
+post('/venues/add') do
+  venue_name = params.fetch('name')
+  Venue.create({:name => venue_name})
+  redirect back
+end
+
+delete('/bands/:id/delete') do
+  band_id = params.fetch('id').to_i()
+  band_to_delete = Band.find(band_id)
+  band_to_delete.destroy()
+  redirect back
+end
+
+delete('/venues/:id/delete') do
+  venue_id = params.fetch('id').to_i()
+  venue_to_delete = Venue.find(venue_id)
+  venue_to_delete.destroy()
   redirect back
 end
