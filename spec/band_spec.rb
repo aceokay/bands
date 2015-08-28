2e822fff805b8c903cc45525f978208506aeac20
Austin <aokincaid@gmail.com>
2015-08-28 10:30:32 -0700

    Confirm normalization of name input for band class

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -4,4 +4,11 @@ describe(Band) do
   describe('#venues') do
     it { should have_and_belong_to_many(:venues) }
   end
+
+  describe('#create') do
+    it('normalizes text') do
+      new_band = Band.create({:name => "       kendrick lamar "})
+      expect(new_band.name).to(eq("Kendrick Lamar"))
+    end
+  end
 end
