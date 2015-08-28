d1c1522b30624a33f864e8c3cbfe65d7bdd5d539
Austin <aokincaid@gmail.com>
2015-08-28 12:23:28 -0700

    Add update and association function to band page

--- app.rb
+++ app.rb
@@ -18,6 +18,13 @@ get('/venues') do
   erb(:venue_list)
 end
 
+get('/bands/:id') do
+  band_id = params.fetch('id').to_i()
+  @band = Band.find(band_id)
+  @venues = Venue.all()
+  erb(:band_info)
+end
+
 post('/bands/add') do
   band_name = params.fetch('name')
   Band.create({:name => band_name})
@@ -27,6 +34,23 @@ end
 post('/venues/add') do
   venue_name = params.fetch('name')
   Venue.create({:name => venue_name})
+  redirect back
+end
+
+post('/bands/:id/venues/add') do
+  band_id = params.fetch('id').to_i()
+  band = Band.find(band_id)
+  venue_id = params.fetch('venue_id').to_i()
+  venue = Venue.find(venue_id)
+  band.venues().push(venue)
+  redirect back
+end
+
+patch('/bands/:id/update') do
+  band_id = params.fetch('id').to_i()
+  band = Band.find(band_id)
+  new_name = params.fetch('name')
+  band.update({:name => new_name})
   redirect back
 end
 
