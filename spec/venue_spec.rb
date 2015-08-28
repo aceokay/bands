7b769e13b6265aa1b76ddb1cda00a45b61b755b5
Austin <aokincaid@gmail.com>
2015-08-28 10:54:03 -0700

    Add spec and method to verify normalization and uniqueness of saved items to the venue database

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -4,4 +4,18 @@ describe(Venue) do
   describe('#bands') do
     it { should have_and_belong_to_many(:bands) }
   end
+
+  describe('#create') do
+    it('normalizes text') do
+      new_venue = Venue.create({:name => "       the gorge "})
+      expect(new_venue.name).to(eq("The Gorge"))
+    end
+
+    it('does not save a venue that has already been saved to the database') do
+      new_venue = Venue.create({:name => "       the gorge "})
+      new_venue2 = Venue.create({:name => " red rock      "})
+      new_venue3 = Venue.create({:name => "    the gorge "})
+      expect(Venue.all).to(eq([new_venue, new_venue2]))
+    end
+  end
 end
