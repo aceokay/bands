f0170067a15cb77c289ec1b57c69b779f27e8425
Austin <aokincaid@gmail.com>
2015-08-28 11:24:22 -0700

    Add index page funtionality to display all bands on record

--- app.rb
+++ app.rb
@@ -5,5 +5,12 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
+  @bands = Band.all()
   erb(:index)
+end
+
+post('/bands/add') do
+  band_name = params.fetch('name')
+  Band.create({:name => band_name})
+  redirect back
 end
